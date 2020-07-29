948a067ae5b400b5733e1dcd51d6e3c46ba7efb2
durgaprsd04 <durgaprsd04@gmail.com>
2020-07-29 22:00:53 +0530

    all the changes for sites

--- website/js/custom.js
+++ website/js/custom.js
@@ -1,4 +1,57 @@
-function myalert()
+
+function inviteUser()
 {
-   document.getElementById("formId").setAttribute("style", "display:inline");
+  document.getElementById("createEvent").setAttribute("style", "display:none");
+  document.getElementById("InviteUser").setAttribute("style", "display:block");
+  document.getElementById("modifyEvent").setAttribute("style", "display:none");
+  document.getElementById("BuyGiftForInvite").setAttribute("style", "display:none");
+  document.getElementById("RemoveGiftForInvite").setAttribute("style", "display:none");
+  document.getElementById("RespondToInvite").setAttribute("style", "display:none");
+}
+function modifyEvent()
+{
+  document.getElementById("createEvent").setAttribute("style", "display:none");
+  document.getElementById("InviteUser").setAttribute("style", "display:none");
+  document.getElementById("modifyEvent").setAttribute("style", "display:block");
+  document.getElementById("BuyGiftForInvite").setAttribute("style", "display:none");
+  document.getElementById("RemoveGiftForInvite").setAttribute("style", "display:none");
+  document.getElementById("RespondToInvite").setAttribute("style", "display:none");
+}
+function BuyGiftForInvite()
+{
+  document.getElementById("createEvent").setAttribute("style", "display:none");
+  document.getElementById("InviteUser").setAttribute("style", "display:none");
+  document.getElementById("modifyEvent").setAttribute("style", "display:none");
+  document.getElementById("BuyGiftForInvite").setAttribute("style", "display:block");
+  document.getElementById("RemoveGiftForInvite").setAttribute("style", "display:none");
+  document.getElementById("RespondToInvite").setAttribute("style", "display:none");
+}
+
+function RemoveGiftForInvite()
+{
+  document.getElementById("createEvent").setAttribute("style", "display:none");
+  document.getElementById("InviteUser").setAttribute("style", "display:none");
+  document.getElementById("modifyEvent").setAttribute("style", "display:none");
+  document.getElementById("BuyGiftForInvite").setAttribute("style", "display:none");
+  document.getElementById("RemoveGiftForInvite").setAttribute("style", "display:block");
+  document.getElementById("RespondToInvite").setAttribute("style", "display:none");
+}
+
+function RespondToInvite()
+{
+  document.getElementById("createEvent").setAttribute("style", "display:none");
+  document.getElementById("InviteUser").setAttribute("style", "display:none");
+  document.getElementById("modifyEvent").setAttribute("style", "display:none");
+  document.getElementById("BuyGiftForInvite").setAttribute("style", "display:none");
+  document.getElementById("RemoveGiftForInvite").setAttribute("style", "display:none");
+  document.getElementById("RespondToInvite").setAttribute("style", "display:block");
+}
+function createEvent1()
+{
+  document.getElementById("createEvent").setAttribute("style", "display:inline");
+  document.getElementById("InviteUser").setAttribute("style", "display:none");
+  document.getElementById("modifyEvent").setAttribute("style", "display:none");
+  document.getElementById("BuyGiftForInvite").setAttribute("style", "display:none");
+  document.getElementById("RemoveGiftForInvite").setAttribute("style", "display:none");
+  document.getElementById("RespondToInvite").setAttribute("style", "display:none");
 }
