db283b2f9c375f447a77e6f9e801d86f6a72d638
durgaprsd04 <durgaprsd04@gmail.com>
2020-08-06 20:06:37 +0530

    minor changes

--- MarriageGift/dbscripts/sample_data_queries.sql
+++ MarriageGift/dbscripts/sample_data_queries.sql
@@ -7,3 +7,21 @@ insert into OCCASSION_TYPE (occassion_type)
 values('Marriage'),
 ('BirthDay'),
 ('HouseWarming');
+
+
+select * from OCCASSION_TYPE
+
+select NEWID()
+
+select gift_id, gift_name from Gift
+
+select * from Gift
+
+insert into GIFT
+Values(newid(), 4,'Trip to Thailand','30000')
+
+select * from gift_type
+insert into gift_type values('Electronics'),
+('Furniture'),
+('Crockery'),
+('Trips')
